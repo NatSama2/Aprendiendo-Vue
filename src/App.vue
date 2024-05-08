7a45a8a128b0fa1aada82950568db3ec1da7471f
NatSama2 <vet.nat.c@gmail.com>
2024-05-08 16:05:22 -0400

    estilos css

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@ export default {
 </script>
 
 <style>
+/*
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -26,4 +27,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+*/
+
+@import "./assets/css/styles.css";
 </style>
